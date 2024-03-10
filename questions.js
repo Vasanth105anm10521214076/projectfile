d0d62390fa094aed7f6778f897cf767cc43a32ba
Vasanth105anm10521214076 <148739806+Vasanth105anm10521214076@users.noreply.github.com>
2024-03-10 17:30:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,377 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Kitab-I-Nauras is a collection of the poems of",
+    answer: "Ibrahim Adil Shah - II",
+    options: [
+      "Ibrahim Adil Shah - II",
+      "Ahmad =II",
+      "Tajud - Din Firuz",
+      "MUhammad -II"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The Cultural Development is characterised by intellectual, aestheticand spiritual attainment.s Who said the above statement",
+    answer: "S.M. Fairchild",
+    options: [
+      "J.S. Mill",
+      "S.M. Fairchild",
+      "Max Weber",
+      "Tansen"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Buddhist poet Asvaghosa wrote 'Buddha Charita 'in _________ language",
+    answer: "Sanskrit",
+    options: [
+      "Prakrit",
+      "Pali",
+      "Urdu",
+      "Sanskrit"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Choose the incorrect match: (i) Suba - sipahsalar    (ii) Sarkar - Faujdar (iii) pargana - Shiqdar  (iv) Village - Bakhshi",
+    answer: "(iv) only",
+    options: [
+      "(i) and(ii) only",
+      "(ii) only",
+      "(iii) and (iv) only",
+      "(iv) only"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Choose the right answer among type: Which of the 'following statements are true about Eleventh Five Yearplan of India?  (i) 'The main focus of this plan was towards faster and more inclusive growth ,  (ii) This plan stresses that benefits of development should reach all Sections of population.'   (iii) Increased rice production in the eastern part of the country",
+    answer: "(i) and (ii) only",
+    options: [
+      "(i) only",
+      " (i) and (iii) only",
+      "(i) and (ii) only",
+      "Answer not know"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "The largest west flowing river of peninsular India is:?",
+    answer: "Narmada",
+    options: [
+      "Mahi",
+      "Sabarmati",
+      "Narmada",
+      "Luni"
+    ]
+  },
+
+  {
+    numb: 7,
+    question:"Two exactly similar wires are stretched by the same load. their elasticities are in the ratio 5:3. The ratio of their elongations are",
+    answer:"(A) 3:5",
+    options:[
+      "(A) 3:5",
+      "(B) 5:3",
+      "(C) 9:25",
+      "(D) 25:9",
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Human blood grouping was discovered by ?",
+    answer: "Landsteiner",
+    options: [
+      "Landsteiner",
+      "Punett",
+      "Correns",
+      "Muller"
+    ]
+  },
+  
+  {
+    numb: 9,
+    question: "In the collective model of the nucleus, the shape of nuclear core is",
+    answer: "Non-spherical",
+    options: [
+      "Non-spherical",
+      "Spherical",
+      "Semi-spherical",
+      "Circular"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which of the following are correctly paired  (1) Kaprekar - Astrophysicist   (2) Janaki Ammal - Biologist  (3) Debashis Mukherjee - Chemistry  (4) Meghnad Saha - Mathematician ",
+    answer: "2 and 3 are correct",
+    options: [
+      "1 and 4 are correct",
+      "1 and 2 are correct",
+      "2 and 4 are correct ",
+      "2 and 3 are correct"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "World Youth Skills day is celebrated on : ?",
+    answer: "July,15",
+    options: [
+      "March,15",
+      "May,15",
+      "August,15",
+      "July,15"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "The first Indian male Badminton player to win a silver medal under men's singles in the world Badnginton Championship was",
+    answer: "Kidambi Srikanth",
+    options: [
+      "Kidambi Srikanth",
+      "Parpalli Kashyap",
+      "Sai Praneeth",
+      " Pullela Gopichand"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "Which of the following statements are correct? (i) The nominal GS.DP growth of TN`is estimated to be 14.0 percet in 2022-23  (ii) In the TN budget estimate 2022-23, the fiscal deficit is estimated at 3.63 percent of GSDP  (iii) The outstanding debt as a percentage of GSDP is expected to be 27.76 percent in 2022-23",
+    answer: "(i),(ii),and (iii)",
+    options: [
+      "i) only",
+      "ii) only",
+      "(i),(ii),and (iii)",
+      "(ii) (iii)"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Which of the following is NOT a feature of the joint family system?",
+    answer: "Individualistic identity",
+    options: [
+      "Authoritarian structure",
+      "Familistic organisation",
+      "Individualistic identity",
+      "Depth of generations"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Tamilnadu housing board and Tamilnadu urban habitat development board were created in",
+    answer: "1961 and 1970",
+    options: [
+      "1961 and 1970",
+      "1951 and l960 `",
+      "1960 and l951",
+      "1970 and 1960"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Which of the following statement is/are true of Free Drop Back Service Janani Sishu Suraksha Kariyakaram (JSSK)?  (i)JSSK started as a pilot project in Tamil Nadu by TNHSP  (ii)Drop Back vehicle can transport mother with babies upto one year.",
+    answer: "Both (i) and (ii) are true",
+    options: [
+      "Only (i)",
+      "Only (ii)",
+      "Both (i) and (ii) are true",
+      "Both (i) and (ii) are false "
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "Virudhunagar Sankaralinganar, involved in an indefinite hunger strike to rename the Madras State as TamilNadu and died During ,",
+    answer: "1956 October",
+    options: [
+      "1956 July",
+      "1956 October",
+      "1965 October",
+      "1965 October"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "Who was the author of the book Oppressed Hindus?",
+    answer: "M.C. Rajah",
+    options: [
+      "M. Palaniswamy",
+      "P.V. Subramanian",
+      "M.C. Rajah",
+      "R.Veeraiyan"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Which one of the following is not the achievement of Justice-Party Rule?",
+    answer: "Non-Cooperation Movement",
+    options: [
+      "Passing Communal G.O. of 1921 and 1922",
+      "Creating public service commission",
+      "Non-Cooperation Movement",
+      "Answer not Known"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: " Which of the following journal is not associated with Subramaniya Bharathi? .",
+    answer: "New India",
+    options: [
+      "A New Daily",
+      "Vijaya",
+      "India",
+      "New India"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "Who are the ones who brave the FATE? 'Oozhaiyum uppakkam kaanpavar'- yaar?",
+    answer: "Deligent people",
+    options: [
+      "Learned ",
+      "Young people",
+      "Deligent people",
+      "Lazy people"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: " What Thiruvallinvar says are the qualities of an Ambassador",
+    answer: "Purity, Resources, Courage",
+    options: [
+      "Knowledge, Origin, Education",
+      "Love, Knowledge, Clarity of speech",
+      "Duty, Knowing the fitting time, Knowing the place",
+      "Purity, Resources, Courage"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "What is the meaning conveyed through.the word 'extinguish ?' With.ghee extinguish fire! It will prove harder by scandal to extinguish love. ",
+    answer: "Put out",
+    options: [
+      "Life",
+      "Put out",
+      "System",
+      "Expertise"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Which moral text is exclaimed as Velaan Vedham ? ",
+    answer: "Naladiyaar",
+    options: [
+      "Thirukkural",
+      "Asarakovai",
+      "Proverbs",
+      "Naladiyaar"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "In which excavation site Sir.Mortimer Wheelar used scientific and soil layer method ?",
+    answer: "Arikkamedu",
+    options: [
+      "Pallavaram",
+      "Attirampakkam",
+      "Arikkamedu",
+      "Kayyal"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "When the Government of India had decided to appoint a One Man commission of Inquiry to inquire into the circumstances of the disappearance of Netaji Subhas Chandra Bose. In 1945__________'s name was recommended by the then `Chief Justice of India.",
+    answer: "G.D.Khosla",
+    options: [
+      "Srinivasdvaradan",
+      "K.K. Ghosh",
+      "D.K.Roy",
+      "G.D.Khosla"
+    ]
+  },
+
+  {
+    numb: 27,
+    question: "Muhammadan Literary and `Scientific Society of Calcutta was organised by",
+    answer: "Abdul Latif",
+    options: [
+      "Amir Ali",
+      "Dudhu Miyan",
+      "Abdul Latif",
+      "Shariatullah"
+    ]
+  },
+
+  {
+    numb: 28,
+    question: "The objective of Tenancy reform is",
+    answer: "Abolition of Intermediaries",
+    options: [
+      "Abolition of Intermediaries",
+      "Increase Rural Literacy",
+      "Regulation ofMoriey Lenders",
+      " Eradication of Rural indebtedness"
+    ]
+  },
+
+  {
+    numb: 29,
+    question: "",
+    answer: "   ",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
